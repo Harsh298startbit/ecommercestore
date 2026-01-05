d4e08bfb06fac80551c093cf8d4266a7910027a6
Harsh298startbit <harshkukreja@startbitsolutions.com>
2026-01-05 15:15:14 +0530

    second  commit

--- config/boot.rb
+++ config/boot.rb
@@ -1,5 +1,5 @@
+require 'logger' # Fix for Ruby 3.1+ compatibility
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
-require 'logger' # Fix for Ruby 3.1+ compatibility
